7910a2dbc06a9c4cec93f24e9e67a1b67045092c
marco <marco@DESKTOP-BKHS1E9.fibertel.com.ar>
2025-06-10 20:16:28 -0300

    corregi error en la clase mapa, termine la clase objeto

--- src/juego/objetos/Objeto.java
+++ src/juego/objetos/Objeto.java
@@ -1,7 +1,34 @@
-
 package juego.objetos;
 
-
 public class Objeto {
-    
+
+    private String tipo;
+    private boolean recogido;
+
+    //constructor
+    public Objeto(String tipo) {
+        this.tipo = tipo;
+        this.recogido = false; //arranca el false, porque no fue recogido
+    }
+
+    //getter del tipo, puede ser Tarjeta, C4, etc
+    public String getTipo() {
+        return tipo;
+    }
+
+    //metodo para saber si el objeto es recogido
+    public boolean fueRecogido() {
+        return recogido;
+    }
+
+    //metodo para recoger un objeto
+    public void recoger() {
+        this.recogido = true;
+    }
+
+    @Override
+    public String toString() {
+        return tipo + (recogido ? " (recogido)" : "");
+    }
+
 }
