b5610f79199f6ce043c9192d02763986c7d055a7
marco <marco@DESKTOP-BKHS1E9.fibertel.com.ar>
2025-06-09 21:18:26 -0300

    termine las clases mapa,celda, posicion y arranque la clase personaje

--- src/juego/mapa/Mapa.java
+++ src/juego/mapa/Mapa.java
@@ -17,22 +17,68 @@ public class Mapa {
         //llenamos cada celda con una instancia vacia
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                celdas[i][j] = new Celda();
+                celdas[i][j] = new Celda(); //usamos el constructor vacio
             }
         }
     }
 
     //devolvemos la celda en la posicion indicada
     public Celda getCelda(int x, int y) {
-        if (x >= 0 && x < filas && y >= 0 && y < columnas) {
+        if ((x >= 0 && x < filas) && (y >= 0 && y < columnas)) {
             return celdas[x][y];
         }
-        return null;
+        return null; //si esta fuera del mapa
     }
 
-    
     //colocamos un personaje en su posicion inicial
-    public void colocarPersonaje(Personaje nuevoPersonaje){
-        
+    public void colocarPersonaje(Personaje nuevoPersonaje) {
+        //buscamos las posiciones del personaje
+        int x = nuevoPersonaje.getPosicion().getX();
+        int y = nuevoPersonaje.getPosicion().getY();
+
+        Celda celda = getCelda(x, y);
+
+        //verificamos que la celda este libre, antes de usarla
+        if (celda != null && celda.estaLibre()) {
+            celda.setPersonaje(nuevoPersonaje);
+        }
+    }
+
+    //movemos un personaje de una celda a otra
+    public void moverPersonaje(Personaje personaje, int nuevoX, int nuevoY) {
+        //obtenemos la posicion actual del personaje
+        int xActual = personaje.getPosicion().getX();
+        int yActual = personaje.getPosicion().getY();
+
+        //obtenemos las celdas actuales y las de destino
+        Celda origen = getCelda(xActual, yActual);
+        Celda destino = getCelda(nuevoX, nuevoY);
+
+        if (destino != null && destino.estaLibre()) {
+            origen.setPersonaje(null); //sacamos el personaje de la celda actual
+            destino.setPersonaje(personaje); //ponemos el personaje en la nueva celda
+            personaje.setPosicion(new Posicion(nuevoX, nuevoY)); //actualizamos la posicion
+        } else {
+            System.out.println("Movimiento invalido: celda ocupada o fuera del mapa");
+        }
+
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; i < columnas; j++) {
+                Celda celda = celdas[i][j];
+
+                if (celda.getPersonaje() != null) {
+                    System.out.print("P "); //personaje presente
+                } else if (celda.getObjeto() != null) {
+                    System.out.print("O "); //objeto presente
+                } else if (celda.isPuertaBloqueda()) {
+                    System.out.print("X "); //puerta cerrada
+                } else {
+                    System.out.print(". "); //celda vacia
+                }
+            }
+        }
     }
 }
--- src/juego/personajes/Personaje.java
+++ src/juego/personajes/Personaje.java
@@ -1,7 +1,55 @@
-
 package juego.personajes;
 
+import juego.mapa.Posicion;
 
-public class Personaje {
-    
+public abstract class Personaje {
+
+    protected String nombre;
+    protected int vida;
+    protected Posicion posicion;
+
+    //constructor
+    public Personaje(String nombre, int vida, Posicion posicion) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.posicion = posicion;
+    }
+
+    //metodo abstracto, cada personaje que implemente esta clase, tiene que implementar el metodo
+    public abstract void mover();
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public void recibirDanio(int danio) {
+        //restamos el daño recibido a la vida que tenemos
+        this.vida -= danio;
+
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
+    }
+
+    public boolean estaMuerto() {
+        return this.vida <= 0;
+    }
+
 }
