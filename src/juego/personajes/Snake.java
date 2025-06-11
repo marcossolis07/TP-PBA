1830c6a7872fccef7f3ffe75ed78debf9ae6db3f
marco <marco@DESKTOP-BKHS1E9.fibertel.com.ar>
2025-06-11 19:13:33 -0300

    termine clase guardia, agregue un ejemplo en main

--- src/juego/personajes/Snake.java
+++ src/juego/personajes/Snake.java
@@ -12,6 +12,8 @@ public class Snake extends Personaje{
 
     @Override
     public void mover() {
+        //por ahora no implementamos movimiento automatico porque no lo necesita
+        //snake se movera por input desde el menu (se maneja manualmente)
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
