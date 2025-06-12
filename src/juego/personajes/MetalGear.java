d631fc87c3c46980cef1d1a37ff52458071210cc
marco <marco@DESKTOP-BKHS1E9.fibertel.com.ar>
2025-06-11 21:57:05 -0300

    termine clases MetalGear y Snake

--- src/juego/personajes/MetalGear.java
+++ src/juego/personajes/MetalGear.java
@@ -1,42 +1,49 @@
-
 package juego.personajes;
+
 import java.util.Random;
 import juego.mapa.Posicion;
 
+public class MetalGear extends Personaje implements Enemigo {
 
-public class MetalGear extends Personaje implements Enemigo{
-    int min=15;
-    int max=40;
-    private Random random = new Random();
-    int ataqueRandom = min + random.nextInt(max - min + 1);
+    private final int min = 15;
+    private final int max = 40;
+    private final Random random = new Random();
 
-    public MetalGear(String nombre, int vida, Posicion posicion) {
-        super(nombre, vida, posicion);
+    public MetalGear(String nombre, int vida) {
+        super(nombre, vida, null); // le paso null porque no necesita posicion
     }
-    
+
     @Override
     public boolean detectar(Snake snake) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-
+        //siempre esta peleando con snake, asi que lo detecta
+        return true;
     }
-    
+
     @Override
     public void atacar(Snake snake) {
-        
-    }
-    
-    public void atacarMisil(Snake snake){
-        snake.recibirDanio(ataqueRandom);
-        System.out.println("¡Metal Gear ataca con un Misil! (-"+ ataqueRandom +" HP)\n");
+        //elegimos aleatoriamente si usa laser o msil
+        if (random.nextBoolean()) {
+            atacarLaser(snake);
+        } else {
+            atacarMisil(snake);
+        }
     }
 
-    public void atacarLaser (Snake snake){
-        snake.recibirDanio(ataqueRandom);
-        System.out.println("¡Metal Gear ataca con un Canion Laser! (-"+ ataqueRandom +" HP)\n");
-    }  
-    
+    public void atacarMisil(Snake snake) {
+        int ataque = min + random.nextInt(max - min + 1);
+        snake.recibirDanio(ataque);
+        System.out.println("¡Metal Gear ataca con un Misil! (-" + ataque + " HP)\n");
+    }
+
+    public void atacarLaser(Snake snake) {
+        int ataque = min + random.nextInt(max - min + 1);
+        snake.recibirDanio(ataque);
+        System.out.println("¡Metal Gear ataca con un Canion Laser! (-" + ataque + " HP)\n");
+    }
+
     @Override
     public void mover() {
+        //no se mueve en el mapa
     }
-    
+
 }
--- src/juego/personajes/Snake.java
+++ src/juego/personajes/Snake.java
@@ -1,20 +1,70 @@
-
 package juego.personajes;
 
+import java.util.Random;
 import juego.mapa.Posicion;
+import juego.objetos.Objeto;
 
+/*
+Protagonista principal, se mueve, recoge objetos, y pelea
+ */
+public class Snake extends Personaje {
 
-public class Snake extends Personaje{
+    private Random random; //para generar numeros aleatorios
+    private boolean tieneTarjeta;
+    private boolean tieneBomba; //seria el C4
 
+    //constructor
     public Snake(String nombre, int vida, Posicion posicion) {
         super(nombre, vida, posicion);
+        this.random = new Random();
+        this.tieneTarjeta = false; //arranca en false porque todavia no tiene nada
+        this.tieneBomba = false; //arranca en false porque todavia no tiene nada
     }
 
     @Override
     public void mover() {
         //por ahora no implementamos movimiento automatico porque no lo necesita
         //snake se movera por input desde el menu (se maneja manualmente)
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println(nombre + " espera órdenes");
     }
-    
+
+    //metodo atacar, para pelear contra MetalGear, daño aleatorio
+    public int atacar() {
+        int danio = random.nextInt(21) + 10; //el daño tiene que ser entre 10 y 30
+
+        System.out.println(nombre + " disparó un misil e hizo " + danio + " de daño.");
+        return danio;
+    }
+
+    //metedo esquivar, para reducir daño
+    public int esquivar(int danioOriginal) {
+        int reduccion = 50 + random.nextInt(51); //entre 50% y 100%
+        int danioReducido = danioOriginal * (100 - reduccion) / 100;
+
+        System.out.println(nombre + " esquivó. Se redujo el daño a " + danioReducido + " HP.");
+        this.recibirDanio(danioReducido); //se aplica el daño recibido
+        return danioReducido;
+    }
+
+    //metodo para recoger objetos
+    public void recogerTarjeta() {
+        this.tieneTarjeta = true;
+        System.out.println("Recogiste la tarjeta de acceso.");
+    }
+
+    //metodo para recoger C4
+    public void recogerBomba() {
+        this.tieneBomba = true;
+        System.out.println("Recogiste el C4.");
+    }
+
+    //metodos para verificar si tiene tarjeta o C4
+    public boolean siTieneTarjeta() {
+        return tieneTarjeta;
+    }
+
+    public boolean siTieneBomba() {
+        return tieneBomba;
+    }
+
 }
